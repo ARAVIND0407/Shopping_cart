f134bb3e9d4e1c829e9d7947d86bacdd3d228e53
Aravind <117259925+ARAVIND0407@users.noreply.github.com>
2023-05-21 18:41:59 +0530

    logo added

--- client/FitForMe/src/app/user/user-routing.module.ts
+++ client/FitForMe/src/app/user/user-routing.module.ts
@@ -4,7 +4,6 @@ import { UserLayoutComponent } from './user-layout/user-layout.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
-import { UserHomePageComponent } from './user-home-page/user-home-page.component';
 
 const routes: Routes = [{
   path: '', component: UserLayoutComponent, children: [
